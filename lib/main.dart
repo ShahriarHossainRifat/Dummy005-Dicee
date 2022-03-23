ed96b44b46bbb91a592fda148c91fc1512e30f40
Shahriar Rifat <shahriarrifat0@gmail.com>
2022-03-24 02:06:25 +0600

    randomized

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
       backgroundColor: Colors.red,
       appBar: AppBar(
@@ -13,28 +15,41 @@ void main() {
   ));
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({Key? key}) : super(key: key);
 
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceImageNumber = 1;
+  int rightDiceImageNumber = 1;
+  void randomizeDice() {
+    setState(() {
+      leftDiceImageNumber = Random().nextInt(6) + 1;
+      rightDiceImageNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    int LeftDiceImageNumber = 2;
     return Center(
       child: Row(
         children: [
           Expanded(
             child: TextButton(
-              child: Image.asset('images/dice$LeftDiceImageNumber.png'),
+              child: Image.asset('images/dice$leftDiceImageNumber.png'),
               onPressed: () {
-                print('Left button pressed!');
+                randomizeDice();
               },
             ),
           ),
           Expanded(
             child: TextButton(
-              child: Image.asset('images/dice2.png'),
+              child: Image.asset('images/dice$rightDiceImageNumber.png'),
               onPressed: () {
-                print('Right button pressed!');
+                randomizeDice();
               },
             ),
           ),
